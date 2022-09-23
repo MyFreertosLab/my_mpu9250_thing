63481daede5df1dc187953618952568b7beec16f
Andrea Lambruschini <andrea.lambruschini@telecomitalia.it>
2022-09-23 18:58:44 +0200

    Aggiunto drivers per imu ed un semplice task per il ciclo di lettura

--- main/main.c
+++ main/main.c
@@ -4,39 +4,98 @@
 #include "esp_event.h"
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
-#include "driver/gpio.h"
+#include "esp_log.h"
+#include "my_mpu9250_task.h"
+
+static const char *TAG = "my_wsclient_thing";
+
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+	switch(event_id) {
+	case WIFI_EVENT_STA_CONNECTED: {
+//        wifi_event_sta_connected_t* event = (wifi_event_sta_connected_t*) event_data;
+        ESP_LOGI(TAG, "CONNECTED");
+		break;
+	}
+	case WIFI_EVENT_STA_DISCONNECTED: {
+//        wifi_event_sta_disconnected_t* event = (wifi_event_sta_disconnected_t*) event_data;
+        ESP_LOGI(TAG, "DISCONNECTED");
+        esp_err_t ret = esp_wifi_connect();
+        if(ret != ESP_OK) {
+        	ESP_LOGE(TAG, "Sorry!! not connected to wifi ...");
+        } else {
+        	ESP_LOGI(TAG, "Connected!");
+        }
+		break;
+	}
+	case WIFI_EVENT_STA_START: {
+        ESP_LOGI(TAG, "START");
+        esp_err_t ret = esp_wifi_connect();
+        if(ret != ESP_OK) {
+        	ESP_LOGE(TAG, "Sorry!! not connected to wifi ...");
+        } else {
+        	ESP_LOGI(TAG, "Connected!");
+        }
+		break;
+	}
+	case WIFI_EVENT_STA_STOP: {
+        ESP_LOGI(TAG, "STOP");
+		break;
+	}
+	default: {
+        ESP_LOGI(TAG, "WIFI received event [%d]", event_id);
+		break;
+	}
+	}
+}
+
+
 void app_main(void)
 {
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("TRANS_TCP", ESP_LOG_DEBUG);
+
     nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+														NULL,
+                                                        NULL));
+
     wifi_config_t sta_config = {
         .sta = {
+        	.channel = 12,
+			.pmf_cfg.capable = false,
             .ssid = CONFIG_ESP_WIFI_SSID,
+			.scan_method = WIFI_ALL_CHANNEL_SCAN,
             .password = CONFIG_ESP_WIFI_PASSWORD,
             .bssid_set = false
         }
     };
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
+    		CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(my_mpu9250_task, "my_mpu9250_task", 4096, NULL, 5, NULL);
+
 }
 
---
+++ main/my_mpu9250_task.c
@@ -0,0 +1,125 @@
+/*
+ * my_mp9250_task.c
+ *
+ *  Created on: 29 gen 2021
+ *      Author: andrea
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <nvs.h>
+#include <driver/spi_master.h>
+#include <driver/gpio.h>
+
+#include <my_mpu9250_task.h>
+#include <mpu9250_accel.h>
+#include <mpu9250_gyro.h>
+#include <mpu9250_mag.h>
+#include <mpu9250_baro.h>
+
+esp_err_t my_mpu9250_init_flash(){
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+	return err;
+}
+
+
+void my_mpu9250_task_init(mpu9250_handle_t mpu9250) {
+	ESP_ERROR_CHECK(my_mpu9250_init_flash());
+}
+
+void my_mpu9250_temperature_read_data_cycle(mpu9250_handle_t mpu9250_handle) {
+	const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 500 );
+	uint32_t counter = 0;
+
+	while (true) {
+		counter++;
+		if( ulTaskNotifyTake( pdTRUE,xMaxBlockTime ) == 1) {
+			ESP_ERROR_CHECK(mpu9250_load_raw_data(mpu9250_handle));
+			// TODO: probabilmente offset non è 0. Verificare
+			if(counter%100 == 0) {
+				float temp_deg = mpu9250_handle->data.raw_data.data_s_xyz.temp_data/333.87 + 21;
+
+				printf("Temperature: [%2.3f][%d]\n", temp_deg, mpu9250_handle->data.raw_data.data_s_xyz.temp_data);
+			}
+	    } else {
+	    	ESP_ERROR_CHECK(mpu9250_test_connection(mpu9250_handle));
+			if(counter%100 == 0) {
+		    	printf("SORRY!! Interrupt LOST!\n");
+			}
+	    }
+	}
+}
+
+static esp_err_t mpu9250_discard_messages(mpu9250_handle_t mpu9250_handle, uint16_t num_msgs) {
+	printf("Discarding %d Samples ... \n", num_msgs);
+	for(uint16_t i = 0; i < num_msgs; i++) {
+		ulTaskNotifyTake( pdTRUE,pdMS_TO_TICKS( 500 ) );
+	}
+	return ESP_OK;
+}
+
+void my_mpu9250_read_data_cycle(mpu9250_handle_t mpu9250_handle) {
+	const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 500 );
+	uint32_t counter = 0;
+	ESP_ERROR_CHECK(mpu9250_gyro_set_fsr(mpu9250_handle, INV_FSR_1000DPS));
+	ESP_ERROR_CHECK(mpu9250_acc_set_fsr(mpu9250_handle, INV_FSR_8G));
+    ESP_ERROR_CHECK(mpu9250_mag_set_mode2_with_precision(mpu9250_handle, INV_MAG_PRECISION_16_BITS));
+	ESP_ERROR_CHECK(mpu9250_mag_set_continuous_reading(mpu9250_handle));
+	ESP_ERROR_CHECK(mpu9250_discard_messages(mpu9250_handle, 10000));
+
+	while (true) {
+		counter++;
+		if( ulTaskNotifyTake( pdTRUE,xMaxBlockTime ) == 1) {
+			ESP_ERROR_CHECK(mpu9250_load_data(mpu9250_handle));
+			if(counter%100 == 0) {
+				printf("Gyro.: S[%d][%d][%d] X[%d][%d][%d]\n", mpu9250_handle->data.gyro.cal.kalman[X_POS].sample, mpu9250_handle->data.gyro.cal.kalman[Y_POS].sample, mpu9250_handle->data.gyro.cal.kalman[Z_POS].sample, mpu9250_handle->data.gyro.cal.kalman[X_POS].X , mpu9250_handle->data.gyro.cal.kalman[Y_POS].X, mpu9250_handle->data.gyro.cal.kalman[Z_POS].X);
+				printf("Accel: S[%d][%d][%d] X[%d][%d][%d]\n", mpu9250_handle->data.accel.cal.kalman[X_POS].sample, mpu9250_handle->data.accel.cal.kalman[Y_POS].sample, mpu9250_handle->data.accel.cal.kalman[Z_POS].sample, mpu9250_handle->data.accel.cal.kalman[X_POS].X , mpu9250_handle->data.accel.cal.kalman[Y_POS].X, mpu9250_handle->data.accel.cal.kalman[Z_POS].X);
+				printf("Mag: S[%d][%d][%d] X[%2.3f][%2.3f][%2.3f] [%3.3f]\n", mpu9250_handle->data.mag.cal.kalman[X_POS].sample, mpu9250_handle->data.mag.cal.kalman[Y_POS].sample, mpu9250_handle->data.mag.cal.kalman[Z_POS].sample, mpu9250_handle->data.mag.body_frame_data.array[X_POS], mpu9250_handle->data.mag.body_frame_data.array[Y_POS], mpu9250_handle->data.mag.body_frame_data.array[Z_POS], mpu9250_handle->data.mag.module);
+				printf("AG-RPY[%2.2f][%2.2f][%2.2f]\n", mpu9250_handle->data.gyro.rpy.xyz.x*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.gyro.rpy.xyz.y*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.gyro.rpy.xyz.z*(double)360.0f/(double)6.283185307f);
+				printf("AA-RPY[%2.2f][%2.2f][%2.2f]\n", mpu9250_handle->data.accel.rpy.xyz.x*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.accel.rpy.xyz.y*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.accel.rpy.xyz.z*(double)360.0f/(double)6.283185307f);
+				printf("AM-RPY[%2.2f][%2.2f][%2.2f]\n", mpu9250_handle->data.mag.rpy.xyz.x*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.mag.rpy.xyz.y*(double)360.0f/(double)6.283185307f, mpu9250_handle->data.mag.rpy.xyz.z*(double)360.0f/(double)6.283185307f);
+//				printf("KG[%1.5f][%1.5f][%1.5f]\n", mpu9250_handle->gyro.cal.kalman[X_POS].K, mpu9250_handle->gyro.cal.kalman[Y_POS].K, mpu9250_handle->gyro.cal.kalman[Z_POS].K );
+			}
+	    } else {
+	    	ESP_ERROR_CHECK(mpu9250_test_connection(mpu9250_handle));
+			if(counter%100 == 0) {
+		    	printf("SORRY!! Interrupt LOST!\n");
+			}
+	    }
+	}
+}
+
+void my_mpu9250_task(void *arg) {
+	const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 500 );
+	// MPU9250 Handle
+	mpu9250_init_t mpu9250;
+	mpu9250_handle_t mpu9250_handle = &mpu9250;
+	my_mpu9250_task_init(mpu9250_handle);
+
+	// Init MPU9250
+	ESP_ERROR_CHECK(mpu9250_init(mpu9250_handle));
+
+	// Calibration
+	// calibration offset and biases
+//	ESP_ERROR_CHECK(mpu9250_calibrate(mpu9250_cal_handle));
+//	ESP_ERROR_CHECK(mpu9250_save_calibration_data(mpu9250_cal_handle));
+
+	// load circular buffer
+	for(uint8_t i = 0; i < CIRCULAR_BUFFER_SIZE; i++) {
+		if( ulTaskNotifyTake( pdTRUE,xMaxBlockTime ) == 1) {
+			ESP_ERROR_CHECK(mpu9250_load_raw_data(mpu9250_handle));
+		}
+	}
+	my_mpu9250_read_data_cycle(mpu9250_handle);
+}

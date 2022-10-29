d14dd39fa72ad4ab9ae9ca11a07af95ef1e921ee
Andrea Lambruschini <andrea.lambruschini@telecomitalia.it>
2022-10-29 11:02:38 +0200

    Aggiunto altr implementazione per confronto

--- examples/ellipse2d_mag.r
+++ examples/ellipse2d_mag.r
@@ -12,6 +12,7 @@ options(rgl.printRglwidget = TRUE)
 # mvmeta for symmetric matrix vectorization (vechMat)
 library(mvmeta)
 library(quadprog)
+library(expm)
 
 # axis correction
 imu.data.body.original <- imu.data.mag %>% select(MY, MX, MZ, MV) %>% filter(MV == 1) %>% select(MY, MX, MZ) %>% rename(MX = MY, MY = MX) %>% mutate(MZ = -MZ)
@@ -24,27 +25,28 @@ rglwidget()
 
 # create model
 imu.data.mag.poly <- imu.data.body %>% select(MX, MY, MZ) %>% mutate(One = -1, MX2 = MX**2, MY2 = MY**2, MZ2=MZ**2, MXY=MX*MY, MXZ=MX*MZ, MYZ=MY*MZ)
-fit <- lm(One ~ . - 1, imu.data.mag.poly)
-coeff.hat <- coef(fit)
-P <- as.matrix(imu.data.mag.poly %>% select(-One))
+mag_model <- lm(One ~ . - 1, imu.data.mag.poly)
+mag_model_coeff.hat <- coef(mag_model)
+mag_model_data <- as.matrix(imu.data.body, ncol=3)
 
-a=coeff.hat["MX2"]
-b=coeff.hat["MXY"]/2
-c=coeff.hat["MXZ"]/2
-e=coeff.hat["MYZ"]/2
-d=coeff.hat["MY2"]
-f=coeff.hat["MZ2"]
+a=mag_model_coeff.hat["MX2"]
+b=mag_model_coeff.hat["MXY"]/2
+c=mag_model_coeff.hat["MXZ"]/2
+e=mag_model_coeff.hat["MYZ"]/2
+d=mag_model_coeff.hat["MY2"]
+f=mag_model_coeff.hat["MZ2"]
 
-A = xpndMat(c(a,b,c,d,e,f))
-B = matrix(c(coeff.hat["MX"],coeff.hat["MY"],coeff.hat["MZ"]))
+mag_model_Q = xpndMat(c(a,b,c,d,e,f))
+mag_model_u = matrix(c(mag_model_coeff.hat["MX"],mag_model_coeff.hat["MY"],mag_model_coeff.hat["MZ"]))
+mag_model_k = 1
 X=as.matrix(imu.data.mag.poly %>% select(MX, MY, MZ))
-eigen(A)
-D=matrix(c(eigen(A)$values[1], 0,0, 0,eigen(A)$values[2],0,0,0,eigen(A)$values[3]), nrow=3, ncol=3)
-NV=eigen(A)$vectors/norm(eigen(A)$vectors, "F")
-V=eigen(A)$vectors
+eigen(mag_model_Q)
+D=matrix(c(eigen(mag_model_Q)$values[1], 0,0, 0,eigen(mag_model_Q)$values[2],0,0,0,eigen(mag_model_Q)$values[3]), nrow=3, ncol=3)
+NV=eigen(mag_model_Q)$vectors/norm(eigen(mag_model_Q)$vectors, "F")
+V=eigen(mag_model_Q)$vectors
 TA=V%*%D%*%t(V)
-stopifnot(abs(A - TA) < 1e-15)
-TD=t(V)%*%A%*%V
+stopifnot(abs(mag_model_Q - TA) < 1e-15)
+TD=t(V)%*%mag_model_Q%*%V
 stopifnot(abs(D - TD) < 1e-15)
 
 # calc center
@@ -55,7 +57,7 @@ stopifnot(abs(D - TD) < 1e-15)
 #             Le soluzioni forniscono il centro
 # NOTA: si è posto F = 1, i coefficienti sono normalizzati rispetto al valore reale di F
 #       Trovare un modo per stimare F
-LB=t(B)%*%V
+LB=t(mag_model_u)%*%V
 Dmat <- 2*D
 dvec <- -LB
 Amat <- matrix(0,3,3)
@@ -171,267 +173,56 @@ scatter3D(imu.data.mag.sphere.final$MX, imu.data.mag.sphere.final$MY, imu.data.m
 plotrgl()
 rglwidget()
 
-#################################################################################################################################################
+#########################################################################################################################################
+# from Complete Triaxis Magnetometer Caibration (is another implementation of the same problem)
+#########################################################################################################################################
+#center
+mag_model_b = -1/2*solve(mag_model_Q)%*%mag_model_u
+#matrix square root of Q
+appo = mag_model_Q %*% t(mag_model_Q)
+appo_eigen = eigen(appo)
+mag_model_V = appo_eigen$vectors
+mag_model_D = matrix(0,3,3)
+diag(mag_model_D) <- appo_eigen$values
+mag_model_magnetic_norm = t(mag_model_u) %*% solve(mag_model_Q) %*% mag_model_u -4*mag_model_k # definire Hm^2 norma del campo magentico terrestre locale. Il valore è fittizio.
+mag_model_inv_A = sqrtm(mag_model_Q)
+mag_model_A = solve(mag_model_inv_A)
+mag_model_data_transformed <- mag_model_data
 
-# Calcola espressione algebrica
-matrix(colSums(coeff.hat * t(imu.data.mag.poly %>% select(MX, MY, MZ, MX2, MY2, MZ2, MXY, MXZ, MYZ))))[1]
-
-# Stesso calcolo in algebra lineare
-t(matrix(X[1,1:3])) %*% A %*% matrix(X[1,1:3]) + t(B) %*% matrix(X[1,1:3])
-
-# Provo il calcolo con tutti gli items
-matrix(X, nrow=dim(X)[1], ncol = dim(X)[2])
-
-# TODO: continuare
-colSums(t(matrix(X,nrow = dim(X)[1], ncol = dim(X)[2]) * t(matrix(A %*% t(matrix(X, nrow = dim(X)[1], ncol = dim(X)[2])), nrow = dim(X)[2], ncol = dim(X)[1])))) + t(t(B) %*% t(matrix(X, dim(X)[1], dim(X)[2])))
-
-C=-0.5*solve(A)%*%B
-AE=1/(t(C)%*%A%*%C+1)[1,1]*A
-
-# from Complete TriAxis Magnetometer Calibration
-# Attenzione: 
-#   per la definizione del symmetric kronecker vedere: https://it.mathworks.com/matlabcentral/answers/361452-symmetric-kronecker-product-in-matlab
-#   0.5U%*%(kronecker(A,B) + kronecker(B,A))%*%t(U)
-#   dove U è definita esplicitamente:
-#   1 0 0 0 0 0 0 0 0
-#   0 1/sqrt(2) 0 1/sqrt(2) 0 0 0 0 0 
-#   0 0 1/sqrt(2) 0 0 0 1/sqrt(2) 0 0
-#   0 0 0 0 1 0 0 0 0 
-#   0 0 0 0 0 1/sqrt(2) 0 1/sqrt(2) 0
-#   0 0 0 0 0 0 0 0 1
-# Non sembra fornire il risultato corretto.
-U=matrix(c(1, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 1/sqrt(2), 0, 1/sqrt(2), 0, 0, 0, 0, 0,
-           0, 0, 1/sqrt(2), 0, 0, 0, 1/sqrt(2), 0, 0,
-           0, 0, 0, 0, 1, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 1/sqrt(2), 0, 1/sqrt(2), 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 1), nrow=6, ncol=9)
-
-# Implementazione di Symmetric Kronecker Product per n=3
-U=matrix(c(1, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 1, 0, 1, 0, 0, 0, 0, 0,
-           0, 0, 1, 0, 0, 0, 1, 0, 0,
-           0, 0, 0, 0, 1, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 1, 0, 1, 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 1), nrow=6, ncol=9, byrow = TRUE)
-symm_kronecker <- function(A,B,U) {
-  U %*% kronecker(A,B)
-}
-x=matrix(X[1,1:3])
-xk=symm_kronecker(x,x, U)
-xtAx=t(xk) %*% matrix(vechMat(A))
-t(x)%*%A%*%x
-xtAx
-y=c(xk, X[1,1:3], 1)
-psiOls=matrix(y) %*% t(matrix(y))
-Uno <- matrix(c(1,1,1,1))
-Indx <- matrix(c(1,2,3,0))
-M = matrix(c(vechMat(Indx %*% t(Uno)), vechMat(Uno %*% t(Indx))), ncol = 2, byrow = FALSE)
-
-########################################################################
-## Algorithms for adjusted least squares estimation
-########################################################################
-###############################
-## Step 1): Form Tensor T 5xnxm
-###############################
-t0 <- function(x, variance) {
-   1
-}
-t1 <- function(x, variance) {
-  x
-}
-t2 <- function(x, variance) {
-  x^2 - variance
-}
-t3 <- function(x, variance) {
-  x^3 - 3*x*variance
-}
-t4 <- function(x, variance) {
-  x^4 - 3*x^2*variance + 3*variance^2
-}
-T <- function(k, i, l, X, variance) {
-    stopifnot(k >= 0 && k <= 4)
-    res=switch(as.integer(k+1), t0(X[l,i], variance), t1(X[l,i], variance),t2(X[l,i], variance),t3(X[l,i], variance),t4(X[l,i], variance))
-    as.double(res)
-}
-###############################
-## Step 2): Define index matrix
-###############################
-make_index_matrix <- function(n) {
-  Uno <- matrix(rep(1,n+1))
-  Indx <- matrix(c(c(1:n),0))
-  matrix(c(vechMat(Indx %*% t(Uno)), vechMat(Uno %*% t(Indx))), ncol = 2, byrow = FALSE)
-}
-#####################################
-## Step 3): Form Tensor R 
-#####################################
-is_equal <- function(v1, v2) {
-  as.integer(v1 == v2)
-}
-M <- make_index_matrix(3)
-R <- function(p,q,i, M) {
-  is_equal(M[p,1], i) + is_equal(M[p,2], i)  + is_equal(M[q,1], i)  + is_equal(M[q,2], i) 
-}
-#####################################
-## Step 4): Compute ni_als
-#####################################
-ni_als <- function(p,q,X,variance,M) {
-   m = dim(X)[1]
-   n = dim(X)[2]
-   s = 0
-   for(l in 1:m) {
-     f = 1
-     for(i in 1:n) {
-       f = f*T(R(p,q,i,M),i,l,X,variance)
-     }
-     s = s + f
-   }
-   s
-}
-#####################################
-## Step 5): Define index off-diagonal
-#####################################
-index_off_diagonal <- function(n) {
-  D1 <- seq(1,(n+1)*n/2)
-  D2 <- seq(1,n)*(seq(1,n)+1)/2
-  setdiff(D1, D2)
+##### T.B.D: L'algoritmo richiede un secondo passaggio per calcolare scale_factors_3 ed applicarlo alla trasformazione
+#####        affinché sia realmente sferico
+for(i in 1:dim(mag_model_data)[1]) {
+  mag_model_data_transformed[i,] = scale_factors_3 %*% mag_model_inv_A %*% t(mag_model_data[i,] - t(mag_model_b))
 }
 
-#####################################
-## Step 6): form matrix psi_als
-#####################################
-make_psi_als <- function(X,variance,M) {
-  n = dim(X)[2]
-  n1 = dim(M)[1]
-  D = index_off_diagonal(n)
-  psi_als = matrix(nrow = n1, ncol = n1)
-  for(p in 1:n1) {
-    for(q in p:n1) {
-      if(is.element(p,D) && is.element(q,D)) {
-        psi_als[p,q] = 4*ni_als(p,q,X,variance, M)
-      } else if(!is.element(p,D) && !is.element(q,D)) {
-        psi_als[p,q] = ni_als(p,q,X,variance, M)
-      } else {
-        psi_als[p,q] = 2*ni_als(p,q,X,variance, M)
-      }
-      psi_als[q,p] = psi_als[p,q]
-    }
-  }
-  psi_als
-}
-psi_als = make_psi_als(X, 315.70,make_index_matrix(3))
+radius_data <- sqrt(as.matrix(mag_model_data_transformed[,1]^2)+as.matrix(mag_model_data_transformed[,2]^2)+as.matrix(mag_model_data_transformed[,3]^2))
 
-################################################
-## Step 7)8): Find eigenvector of min eigenvalue
-################################################
-calc_eigenvector_psi_als <- function(psi_als) {
-  eigen_als = eigen(psi_als)
-  eigen_values = eigen_als$values
-  eigen_vectors = eigen_als$vectors 
-  idx = which.min(eigen_values)
-  beta = matrix(eigen_vectors[, idx])
-  beta/norm(beta)
-}
-################################################
-## Step 9): Estimate A, b, d
-################################################
-estimate_A <- function(beta, n) {
-  xpndMat(beta[1:(n*(n+1)/2)])
-}
-estimate_b <- function(beta, n) {
-  beta[(n*(n+1)/2+1):(dim(beta)[1]-1)]
-}
-estimate_d <- function(beta, n) {
-  beta[dim(beta)[1]]
-}
-estimate_c <- function(A,b,n) {
-  c=-0.5*solve(estimate_A(psi_als,n))%*%estimate_b(psi_als,n)
-}
-estimate_Ae <- function(A,c,d) {
-   as.double((1/(t(c) %*% A %*% c - d)))*A
-}
-estimate_all <- function(psi_als, n) {
-  beta = calc_eigenvector_psi_als(psi_als)
-  A=estimate_A(beta, n)
-  b=estimate_b(beta, n)
-  d=estimate_d(beta, n)
-  c=estimate_c(A, b, n)
-  Ae=estimate_Ae(A,c,d)
-
-  als = {}
-  als$A=A
-  als$b=b
-  als$d=d
-  als$c=c
-  als$Ae=Ae
-  
-  v1=matrix(eigen(Ae)$vectors[,1])
-  v2=matrix(eigen(Ae)$vectors[,2])
-  v3=matrix(eigen(Ae)$vectors[,3])
-  
-  l1=as.double(eigen(Ae)$values[1])
-  l2=as.double(eigen(Ae)$values[2])
-  l3=as.double(eigen(Ae)$values[3])
-
-  print("autovalori pre")
-  print(l1)
-  print(l2)
-  print(l3)
-  if(l1 < 0 || l2 < 0 || l3 < 0) {
-    Ae2 = matrix(rep(0,n*n), nrow = n, ncol=n)
-    if(l1>0) {
-      Ae2 = Ae2 + l1*(v1%*%t(v1))
-      print("l1")
-    }
-     if(l2>0) {
-      Ae2 = Ae2 + l2*(v2%*%t(v2))
-      print("l2")
-    }
-    if(l3>0) {
-      Ae2 = Ae2 + l3*(v3%*%t(v3))
-      print("l3")
-    }
-    als$Ae = Ae2
-  }  
-  l1=as.double(eigen(als$Ae)$values[1])
-  l2=as.double(eigen(als$Ae)$values[2])
-  l3=as.double(eigen(als$Ae)$values[3])
-  
-  print("autovalori post")
-  print(l1)
-  print(l2)
-  print(l3)
-  als
-}
-
-als=estimate_all(psi_als, 3)
-
-for(i in 1:(dim(X)[1])) {
-  x=matrix(X[i,1:3])
-  val=as.double(t(x)%*%als$A%*%x+t(als$b)%*%x+als$d)
-  if(val > 0) {
-    print(val)
-  }
-}
-
-for(i in 1:(dim(X)[1])) {
-  x=matrix(X[i,1:3])-matrix(als$c)
-  val=as.double(t(x)%*%als$Ae%*%x)
-  if(val < 0) {
-    print(val)
-  }
-}
-Ae_eigen_values=matrix(eigen(als$Ae)$values)
-a=sqrt(as.double(abs(1/Ae_eigen_values[1,1])))
-b=sqrt(as.double(abs(1/Ae_eigen_values[2,1])))
-c=sqrt(as.double(abs(1/Ae_eigen_values[3,1])))
+open3d()
+points3d(mag_model_data_transformed, col = "blue")
+spheres3d(c(0,0,0), radius = 1.803740, col = "red", alpha = 0.4)
+mag_model_data_transformed <- as.data.frame(mag_model_data_transformed)
+scatter3D(mag_model_data_transformed$MX, mag_model_data_transformed$MY, mag_model_data_transformed$MZ, colvar = mag_model_data_transformed$MZ, col = NULL, add = FALSE)
+plotrgl()
+rglwidget()
 
 
-# T.B.D
-Q=AE
-k=t(C) %*% Q %*% C -1
-u=-2*t(AE) %*% C
-h=matrix(X[1,1:3])
-t(h) %*% Q %*% (h) + t(u) %*% h + k
+# Check that it's a sphere
+imu.data.mag.poly <- as.data.frame(mag_model_data_transformed) %>% select(MX, MY, MZ) %>% mutate(One = 1, MX2 = MX**2, MY2 = MY**2, MZ2=MZ**2) %>% select(One, MX2, MY2, MZ2)
+fit_spheric <- lm(One ~ . - 1, imu.data.mag.poly)
+coeff_spheric2.hat <- coef(fit_spheric)
+sphere_axis=sqrt(1/coeff_spheric2.hat )
+sphere_axis
 
+## T.B.D: after the first estimation I need to calculate the scale_factors. Why? 
+## T.B.D: the radius of sphere
+#ellipsoid_axis = matrix(0,3,3)
+#c1=sqrt(1/coeff_spheric2.hat[1])
+#c2=sqrt(1/coeff_spheric2.hat[2])
+#c3=sqrt(1/coeff_spheric2.hat[3])
 
+#diag(ellipsoid_axis) <- (1/((c1*c2*c3)^(2/3)))*c(c2*c3,c1*c3,c1*c2)
+#scale_factors_3 = matrix(0,3,3)
+#diag(scale_factors_3) <- diag(ellipsoid_axis)
+#sphere_radius = (c1*c2*c3)^(1/3)
+#scale_factors_3
+#sphere_radius

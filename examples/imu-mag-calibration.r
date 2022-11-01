04da8be5e767ed07e6ba36bcecf259b8321df1e2
Andrea Lambruschini <andrea.lambruschini@telecomitalia.it>
2022-11-01 20:22:47 +0100

    Corretto imu-mag-calibration.r

--- examples/imu-mag-calibration.r
+++ examples/imu-mag-calibration.r
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(tidyverse)
 library(moderndive)
 library(skimr)
@@ -9,6 +11,8 @@ library(plot3Drgl)
 options(rgl.printRglwidget = TRUE)
 # mvmeta for symmetric matrix vectorization (vechMat)
 library(mvmeta)
+imu.data.mag <- read.csv('/hd/eclipse-cpp-2020-12/eclipse/workspace/my_mpu9250_thing/examples/imu-data-mag.csv')
+
 mag_data_correction <- function(mag_data) {
   # axis correction
   result <- mag_data %>% select(MY, MX, MZ, MV) %>% filter(MV == 1) %>% select(MY, MX, MZ) %>% rename(MX = MY, MY = MX) %>% mutate(MZ = -MZ)
@@ -57,7 +61,7 @@ mag_create_model <- function(mag_data) {
   mag_data_target <- mag_data
   
   # apply model to data
-  for(i in 1:dim(mag_model_data)[1]) {
+  for(i in 1:dim(mag_data)[1]) {
     mag_data_target[i,] = (mag_model_inv_A %*% t(mag_data_source[i,] - t(mag_model_b)))
   }
   mag_data_target <- as.data.frame(mag_data_target)
@@ -101,6 +105,7 @@ mag_create_model <- function(mag_data) {
   result$model <- mag_model
   result$data_source <- mag_data
   result$last_coeff <- coeff_spheric2.hat
+  result$offsets <- mag_model_b
   return(result)
 }
 
@@ -135,9 +140,14 @@ spheres3d(t(as.matrix(mag_model$b)), radius = 227.9377, col = "red", alpha = 0.4
 
 mag_plot_data(mag_data_target, sphere_radius=1)
 
-open3d()
 plot3d(mag_data_target, col = "blue")
 spheres3d(c(0,0,0), radius = 1, col = "red", alpha = 0.4)
+
+
+scatter3D(mag_model$data_source$MX-mag_model$offsets[1], mag_model$data_source$MY-mag_model$offsets[2], mag_model$data_source$MZ-mag_model$offsets[3], col = "green", add = FALSE)
+scatter3D(mag_data_target$MX*250, mag_data_target$MY*250, mag_data_target$MZ*250, col = "red", add = TRUE)
+plotrgl()
+rglwidget()
 
 # Check that it's a sphere
 imu.data.mag.poly <- mag_data_target %>% select(MX, MY, MZ) %>% mutate(One = 1, MX2 = MX**2, MY2 = MY**2, MZ2=MZ**2) %>% select(One, MX2, MY2, MZ2)
